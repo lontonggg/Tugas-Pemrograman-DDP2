ca4ae608c7ad3e817bbf3f6bb5882421146500f3
lontonggg <reyhan.zada@ui.ac.id>
2023-02-27 20:22:46 +0700

    Menyelesaikan fitur 1

--- assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/NotaGenerator.java
@@ -10,6 +10,31 @@ public class NotaGenerator {
      */
     public static void main(String[] args) {
         // TODO: Implement interface menu utama
+        boolean programRuns = true;
+        while(programRuns){
+            printMenu();
+            System.out.print("Pilihan : ");
+            String pilihan = input.nextLine();
+            System.out.println("================================");
+            if(pilihan.equals("1")){
+                System.out.println("Masukkan nama Anda :");
+                String nama = input.next();
+                input.nextLine();
+                System.out.println("Masukkan nomor handphone Anda:");
+                String noHandphone = validasiAngka();
+                System.out.println("ID Anda : " + generateId(nama, noHandphone));
+            }
+            else if(pilihan.equals("2")){
+                // Bikin fitur 2
+            }
+            else if(pilihan.equals("0")){
+                programRuns = false;
+            }
+            else{
+                System.out.println("Perintah tidak diketahui, silakan periksa kembali.");
+            }
+        }
+        System.out.println("Terima kasih telah menggunakan NotaGenerator!");
     }
 
     /**
@@ -42,7 +67,20 @@ public class NotaGenerator {
      */
     public static String generateId(String nama, String nomorHP){
         // TODO: Implement generate ID sesuai soal.
-        return null;
+        int checksum = 0;
+        String gabunganNamaHP = nama.toUpperCase() + "-" + nomorHP;
+        for(int i = 0; i < gabunganNamaHP.length(); i++){
+            if(gabunganNamaHP.charAt(i) >= 65 && gabunganNamaHP.charAt(i) <= 90){
+                checksum += gabunganNamaHP.charAt(i) - 'A' + 1;
+            }
+            else if(Character.isDigit(gabunganNamaHP.charAt(i))){
+                checksum += Character.getNumericValue(gabunganNamaHP.charAt(i));
+            }
+            else{
+                checksum += 7;
+            }
+        }
+        return String.format("%s-%02d", gabunganNamaHP, checksum);
     }
 
     /**
@@ -62,5 +100,26 @@ public class NotaGenerator {
     public static String generateNota(String id, String paket, int berat, String tanggalTerima){
         // TODO: Implement generate nota sesuai soal.
         return null;
+    }
+
+    public static String validasiAngka(){
+        String angka;
+        boolean angkaValid = false;
+        while(true){
+            angka = input.nextLine();
+            for(int i = 0; i < angka.length(); i++){
+                if(Character.isDigit(angka.charAt(i))){
+                    angkaValid = true;
+                }
+                else{
+                    System.out.println("Nomor HP hanya menerima digit");
+                    angkaValid = false;
+                    break;
+                }
+            }
+            if(angkaValid){
+                return angka;
+            }
+        }
     }
 }
